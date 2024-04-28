1f9e7acca61cd86511ec6b8c44b3fd6b7f748b68
Shaka S. Dow <shakad@yahoo.com>
2024-04-27 21:26:29 -0400

    Improved validation

--- js/global.js
+++ js/global.js
@@ -1,11 +1,36 @@
-var contact_dialog = document.getElementsByTagName('dialog')[0];
-var contact_form = document.getElementsByTagName('form')[0];
+// Global variables
+const contact_dialog = document.getElementsByClassName('contact_dialog')[0];
+const contactSubmitted_dialog = document.getElementsByClassName('contact_submitted_dialog')[0];
+const contact_form = document.getElementsByTagName('form')[0];
+const pageList = ['home','our_team','testimonials'];
 
 /*
-Function that is called when the 'Contact Us' button is clicked
+Function that reveals the contact dialog
 */
 function showContactForm() {
     contact_dialog.showModal();
+}
+
+/*
+Function that closes the contact dialog and clear its form contents
+*/
+function closeContactForm() {
+    contact_dialog.close();
+    contact_form.reset();
+}
+
+/*
+Function that reveals the contact submitted dialog
+*/
+function showContactSubmittedForm(){
+    contactSubmitted_dialog.showModal();
+}
+
+/*
+Function that closes the contact submitted dialog
+*/
+function closeContactSubmittedForm(){
+    contactSubmitted_dialog.close();
 }
 
 /*
@@ -18,31 +43,28 @@ function submitContactForm() {
         // Form validation passed
         // Rather than submit using AJAX, simply close the form for now
         contact_dialog.close();
+        contact_form.reset();
+        contactSubmitted_dialog.showModal();
+        return false;
     }else{
         // Form validation failed. Do nothing for now
     }
 }
 
 /*
-Function that is called when the 'close' button is clicked 
-on the contact form
-*/
-function closeContactForm() {
-    contact_dialog.close();
-    // Clear any residual content
-}
-
-/*
 Function that is called after page content has been loaded.
-Will acquire the value of the 'page' parameter and use AJAX to load
+Will acquire the value of the 'page' search parameter and use AJAX to load
 the content.
 */
 window.onload = function () {
-    // acquire parameter
+    // acquire search parameters specified in URL
     const searchParams = new URLSearchParams(window.location.search);
+    // Is the 'page' search parameter specified?
     if (searchParams.has('page')) {
+        // Yes the 'page' searcn parameter is specified. Attempt to load content using AJAX
         AJAXload(searchParams.get('page'));
     } else {
+        // No the 'page' search parameter is NOT specified. Load pages/home.html
         AJAXload('home');
     }
 
@@ -51,25 +73,19 @@ window.onload = function () {
 /*
 Function that is called after the 'page' parameter has been acquired
 */
-function AJAXload(page) {
-    // highlight the appropriate button in the header section
-    if (page.substr(0, 8) === 'details_') {
-        document.getElementById('btn_home').className = 'btn_active';
-    } else {
-        // Clear classes from buttons first
-        let liElements = document.getElementsByTagName('ul')[0].children;
-        for(x=0;x<liElements.length;x++){
-            liElements[x].className="";
+function AJAXload(pageParam) {
+    // Highlight the appropriate button in the header section
+    pageList.forEach(function(page){
+        if(pageParam.substr(0,page.length)===page){
+            document.getElementById('btn_'+page).className='btn_active';
         }
-        document.getElementById('btn_' + page).className = 'btn_active';
-    }
-
-    // attemp AJAX call
+    });
+    // Attemp AJAX call
     if (window.XMLHttpRequest) {
         request = new XMLHttpRequest();
         try {
             request.onreadystatechange = getInfo;
-            request.open("GET", 'pages/' + page + '.html', true);
+            request.open("GET", 'pages/' + pageParam + '.html', true);
             request.send();
         } catch (e) {
             alert("Unable to connect to server");
@@ -82,11 +98,16 @@ function AJAXload(page) {
 
 /*
 Function that is called when the AJAX call has been successfully completed.
-Will insert the loaded content into the DIV element with ID 'content'.
+If the requested content was found it will be injected inside the element with the 'content' ID
+else the home page will be loaded
 */
 function getInfo() {
     if (request.readyState == 4) {
-        var val = request.responseText;
-        document.getElementById('content').innerHTML = val;
+        if(request.status===200){
+            var val = request.responseText;
+            document.getElementById('content').innerHTML = val;
+        }else{
+            AJAXload('home');
+        }
     }
 }
